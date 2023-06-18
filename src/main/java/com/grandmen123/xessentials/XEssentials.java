dd2c83df70710d2cdeec88d3d2ed91e69e6402b8
Victor Vicente <vitor@bu.edu>
2023-06-18 11:48:29 +0100

    Module Initializers

--- src/main/java/com/grandmen123/xessentials/XEssentials.java
+++ src/main/java/com/grandmen123/xessentials/XEssentials.java
@@ -1,15 +1,53 @@
 package com.grandmen123.xessentials;
 
+import com.grandmen123.xessentials.lib.modules.XModule;
+import com.grandmen123.xessentials.util.Constants;
 import net.fabricmc.api.ModInitializer;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class XEssentials implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("xessentials");
+import java.util.Collections;
+import java.util.List;
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+public class XEssentials implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Constants.ID_CAPS);
+    public static final String MOD_ID = Constants.ID_DEFAULT;
+
+	@NotNull
+    private List<XModule> modules = List.of();
+
+    @Override
+    public void onInitialize() {
+        this.initModules();
+    }
+
+	@NotNull
+	public List<XModule> getModules() {
+		return this.modules;
 	}
+
+
+    private void initModules() {
+		LOGGER.info(Constants.LOG_LOADING_MODULES);
+        XModule xChests;
+        XModule xFurnaces;
+        XModule xEnder;
+        XModule xCompress;
+        XModule xXP;
+        XModule xLights;
+        XModule xMobs;
+        XModule xBuild;
+        XModule xTPUtils;
+        XModule xMisc;
+
+        List<XModule> uninitialized = new java.util.ArrayList<>(List.of());
+		Collections.sort(uninitialized);
+
+		for (XModule mod : uninitialized) {
+			mod.initModule();
+			this.modules.add(mod);
+		}
+    }
 }
